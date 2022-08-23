293b7f45d779b4bff213a7c15ba2acff386fd8b8
Jacob Peat <jacob.peat01@gmail.com>
2022-08-23 12:05:30 +0100

    Add a new view getNote and add it to URl patterns

--- api/views.py
+++ api/views.py
@@ -46,3 +46,10 @@ def getNotes(request):
     notes = Note.objects.all()
     serializer = NoteSerializer(notes, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getNote(request, pk):
+    notes = Note.objects.get(id=pk)
+    serializer = NoteSerializer(notes, many=False)
+    return Response(serializer.data)
