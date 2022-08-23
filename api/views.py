e211e21e73777ee00b5011f719303bc4b35a8bfc
Jacob Peat <jacob.peat01@gmail.com>
2022-08-23 11:53:02 +0100

    install django rest framework and use it to render routes

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         {
@@ -34,4 +37,4 @@ def getRoutes(request):
             'description': 'Deletes and exiting note'
         },
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
